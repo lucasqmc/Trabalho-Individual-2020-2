39fa8ea9ee31a615ce2326400012d11247939473
Lucas Leite <lucasleitemm@hotmail.com>
2021-05-03 16:15:50 -0300

    Fix variable name

--- client/src/http.js
+++ client/src/http.js
@@ -5,6 +5,6 @@ const ENDPOINT_DEV = 'http://localhost:8000';
 const enviroment = process.env.NODE_ENV
 const baseURL = enviroment === 'development' ? ENDPOINT_DEV : ENDPOINT_PROD
 
-export const api = axios.create({
+export const http = axios.create({
   baseURL
 });
